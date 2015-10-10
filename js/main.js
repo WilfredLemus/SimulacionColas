6486512372fc482ef8e7b1199d488223e57a4867
WALR <wilfred.lean.15@gmail.com>
2015-10-10 02:05:30 -0600

    formulas mm1

--- js/main.js
+++ js/main.js
@@ -1,23 +1,49 @@
-var n = 1;
-var crTime, TllegadaAnt;
+var n, crTime, TllegadaAnt, HinicioServicio, Hllegada, HfinServicioAnt, HfinServicioAnt, HfinServicio;
+
 $( document ).ready(function() {
     setInterval('reloj()', 1000);
 
     $('#empezar').click(function(){
+        $('#P').text('');
+        $('#PO').text('');
+        $('#LS').text('');
+        $('#WS').text('');
+        $('#LQ').text('');
+        $('#WQ').text('');
+        $('#PN').text('');
+
         crTime = getDate( );
-        // setInterval('process()', 5000);
+        $('.bar').width('0');
         var Ntiempo = parseInt($("#Ntiempo").val());
+        // var Ntiempo = 5;
         crTime.setSeconds(crTime.getSeconds() + Ntiempo);
         n = 1;
+        TllegadaAnt = HinicioServicio = Hllegada = HfinServicioAnt = HfinServicioAnt = HfinServicio = 0;
         $("#table-body tr").remove();
-        TllegadaAnt = 0;
         process();
+        progress(Ntiempo);
+        // setInterval('process()', 5000);
         // setTimeout(process, 5000);
         // sleep(5000);
         // window.setTimeout(process(), 5000);
 
     });
 });
+
+function progress(tm){
+    $('#barpro').show();
+    var ch = 450/(tm+1);
+
+    var pro = setInterval(function () {
+        var $bar = $('.bar');
+        if ($bar.width() >= 450) {
+            clearInterval(pro);
+            // $('.progress').removeClass('active');
+        } else {
+            $bar.width($bar.width() + ch);
+        }
+    }, 1000);
+}
 
 // var crHrs = crTime.getHours ( );
 // var crMns = crTime.getMinutes ( );
@@ -27,44 +53,66 @@ $( document ).ready(function() {
 
 function process(){
     var newTime = new Date ( );
-    var newcrScs = newTime.getSeconds ( );
-    var cnt = 5;
-    var Tllegada = newTime.getSeconds()-TllegadaAnt;
-    var Hllegada = getTime(newTime);
-    // alert("Hola");
-    // console.log("Entro"+crScs);
-    // newclient();
-    // $("#table-body").append('<tr>\
-    //                  <th>'+n+'</th>\
-    //                  <th>'+Tllegada+'</th>\
-    //                  <th>'+Hllegada+'</th>\
-    //                  <th>'+HinicioServicio+'</th>\
-    //                  <th>'+Tservicio+'</th>\
-    //                  <th>'+HfinServicio+'</th>\
-    //                  <th>'+TEspera+'</th>\
-    //                  <th>'+TInactivo+'</th>\
-    //              </tr>');
+    // var newcrScs = newTime.getSeconds ( );
+    var Tllegada = ran();
+    var Tservicio = ran();
+    Hllegada += Tllegada;
+ 
+    if (HfinServicio>=Hllegada) {
+        HinicioServicio = HfinServicio;
+    }else{
+        HinicioServicio = Hllegada;
+    }
 
-    $("#table-body").append('<tr>\
+    HfinServicio = HinicioServicio+Tservicio;
+    var TCola = HinicioServicio - Hllegada;
+    var TEspera = HfinServicio - Hllegada;
+    var TInactivo = HinicioServicio - HfinServicioAnt;
+
+    $("#table-body").append('<tr class="dato">\
             <th>'+n+'</th>\
             <th>'+Tllegada+'</th>\
             <th>'+Hllegada+'</th>\
-            <th>PRUEBA</th>\
-            <th>P</th>\
-            <th>P</th>\
-            <th>P</th>\
-            <th>P</th>\
+            <th>'+HinicioServicio+'</th>\
+            <th>'+Tservicio+'</th>\
+            <th>'+HfinServicio+'</th>\
+            <th>'+TEspera+'</th>\
+            <th>'+TCola+'</th>\
+            <th>'+TInactivo+'</th>\
         </tr>');
     n++;
-    TllegadaAnt = newTime.getSeconds();
-    // sleep(3000);
-    
-    // console.log("Fin de Segundos"+crTime);
-    // console.log("Segundo Actual"+newTime);
+    HfinServicioAnt = HfinServicio;
     if (newTime<=crTime) {
         setTimeout(process, rando(5000));
+    }else{
+        form();
+        $('#barpro').hide();
     }
+
  }
+
+
+function form(){
+    var ProLlegada = ProServicio = ProEspera = ProCajInac = 0;
+    $('#table-body tr').each(function(){
+        ProLlegada += parseInt($(this).find('th').eq(1).text())
+        ProServicio += parseInt($(this).find('th').eq(4).text())
+        ProEspera += parseInt($(this).find('th').eq(6).text())
+        // ProCajInac += parseInt($(this).find('th').eq(8).text())
+    })
+    var ct = n-1;
+    ProLlegada = ProLlegada/ct;
+    // ProServicio = ProServicio/ct;
+    Servicio = ProEspera/ct;
+    $('#P').text((ProLlegada/Servicio).toFixed(2));
+    $('#PO').text((1-(ProLlegada/Servicio)).toFixed(2));
+    $('#LS').text((ProLlegada/(Servicio-ProLlegada)).toFixed(2));
+    $('#WS').text((1/(Servicio-ProLlegada)).toFixed(2));
+    $('#LQ').text(((ProLlegada*ProLlegada)/Servicio*(Servicio-ProLlegada)).toFixed(2));
+    $('#WQ').text(((ProLlegada)/Servicio*(Servicio-ProLlegada)).toFixed(2));
+    $('#PN').text(((1-(ProLlegada/Servicio))*Math.pow((ProLlegada/Servicio), 3)).toFixed(2));
+}
+
 
 function getTime (Time){
     var crTime = Time;
