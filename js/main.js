a9e3434d3c68253589efc3f40cc73c198471229f
WALR <wilfred.lean.15@gmail.com>
2015-10-09 21:31:58 -0600

    Modificaciones de tiempos y procesos

--- js/main.js
+++ js/main.js
@@ -1,56 +1,126 @@
+var n = 1;
+var crTime, TllegadaAnt;
 $( document ).ready(function() {
     setInterval('reloj()', 1000);
 
     $('#empezar').click(function(){
-        var nclient = getClient();
+        crTime = getDate( );
+        // setInterval('process()', 5000);
+        var Ntiempo = parseInt($("#Ntiempo").val());
+        crTime.setSeconds(crTime.getSeconds() + Ntiempo);
+        n = 1;
         $("#table-body tr").remove();
-        if(nclient!=false){
-            var Hllegada = 0;
-            var HfinServicioAnt = 0;
-            var HinicioServicio = 0;
-            for (var i = 1; i <=nclient ; i++) {
-                var Tllegada = ran();
-                var Tservicio = ran();
-                Hllegada += Tllegada;
+        TllegadaAnt = 0;
+        process();
+        // setTimeout(process, 5000);
+        // sleep(5000);
+        // window.setTimeout(process(), 5000);
 
-                if (HfinServicio>=Hllegada) {
-                    HinicioServicio = HfinServicio;
-                }else{
-                    HinicioServicio = Hllegada;
-                }
-                var HfinServicio = HinicioServicio+Tservicio;
-                var TEspera = HfinServicio - Hllegada;
-                var TInactivo = HinicioServicio - HfinServicioAnt;
-
-                $("#table-body").append('<tr>\
-                        <th>'+i+'</th>\
-                        <th>'+Tllegada+'</th>\
-                        <th>'+Hllegada+'</th>\
-                        <th>'+HinicioServicio+'</th>\
-                        <th>'+Tservicio+'</th>\
-                        <th>'+HfinServicio+'</th>\
-                        <th>'+TEspera+'</th>\
-                        <th>'+TInactivo+'</th>\
-                    </tr>');
-
-                HfinServicioAnt = HfinServicio;
-                // console.log('hola--'+i);
-            }
-        }
     });
-
-
 });
 
-function getClient() {
-    var nclient = $("#Nclient").val();
-    if (nclient<=1) {
-        $('#Nclient').popover('show')
-        return false
-    }else{
-        $("#Nclient").val('');
-        return nclient;
+// var crHrs = crTime.getHours ( );
+// var crMns = crTime.getMinutes ( );
+// var crScs = crTime.getSeconds ( );
+// var FinSegundos = crScs+15;
+// crTime.setSeconds(15);
+
+function process(){
+    var newTime = new Date ( );
+    var newcrScs = newTime.getSeconds ( );
+    var cnt = 5;
+    var Tllegada = newTime.getSeconds()-TllegadaAnt;
+    var Hllegada = getTime(newTime);
+    // alert("Hola");
+    // console.log("Entro"+crScs);
+    // newclient();
+    // $("#table-body").append('<tr>\
+    //                  <th>'+n+'</th>\
+    //                  <th>'+Tllegada+'</th>\
+    //                  <th>'+Hllegada+'</th>\
+    //                  <th>'+HinicioServicio+'</th>\
+    //                  <th>'+Tservicio+'</th>\
+    //                  <th>'+HfinServicio+'</th>\
+    //                  <th>'+TEspera+'</th>\
+    //                  <th>'+TInactivo+'</th>\
+    //              </tr>');
+
+    $("#table-body").append('<tr>\
+            <th>'+n+'</th>\
+            <th>'+Tllegada+'</th>\
+            <th>'+Hllegada+'</th>\
+            <th>PRUEBA</th>\
+            <th>P</th>\
+            <th>P</th>\
+            <th>P</th>\
+            <th>P</th>\
+        </tr>');
+    n++;
+    TllegadaAnt = newTime.getSeconds();
+    // sleep(3000);
+    
+    // console.log("Fin de Segundos"+crTime);
+    // console.log("Segundo Actual"+newTime);
+    if (newTime<=crTime) {
+        setTimeout(process, rando(5000));
     }
+ }
+
+function getTime (Time){
+    var crTime = Time;
+    var crHrs = crTime.getHours ( );
+    var crMns = crTime.getMinutes ( );
+    var crScs = crTime.getSeconds ( );
+    var timeOfDay = ( crHrs < 12 ) ? "AM" : "PM";
+    crHrs = ( crHrs > 12 ) ? crHrs - 12 : crHrs;
+    crHrs = ( crHrs == 0 ) ? 12 : crHrs;
+    var crTimeString = crHrs + ":" + crMns + ":" + crScs;
+    return crTimeString;
+    // $("#reloj").html(crTimeString);
+ }
+
+
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
+function newclient(){
+    var Tllegada = ran();
+    var Tservicio = ran();
+    Hllegada = Tllegada;
+
+    // if (HfinServicio>=Hllegada) {
+    //     HinicioServicio = HfinServicio;
+    // }else{
+    //     HinicioServicio = Hllegada;
+    // }
+    // var HfinServicio = HinicioServicio+Tservicio;
+    // var TEspera = HfinServicio - Hllegada;
+    // var TInactivo = HinicioServicio - HfinServicioAnt;
+
+    // $("#table-body").append('<tr>\
+    //         <th>1</th>\
+    //         <th>'+Tllegada+'</th>\
+    //         <th>'+Hllegada+'</th>\
+    //         <th>PRUEBA</th>\
+    //         <th>P</th>\
+    //         <th>P</th>\
+    //         <th>P</th>\
+    //         <th>P</th>\
+    //     </tr>');
+    
+
+    // HfinServicioAnt = HfinServicio;
+}
+
+function getDate () {
+    return new Date ( );
 }
 
 function reloj ( ){
@@ -69,4 +139,8 @@ function reloj ( ){
 
  function ran(){
      return Math.floor((Math.random() * 10) + 1);
+ }
+
+  function rando(tm){
+     return Math.floor((Math.random() * tm) + 1);
  }
